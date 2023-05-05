c040c9b30345aa6848b52d7c62d9f22f3c972021
aliakseidonut <alexeydomut@gmail.com>
2023-05-05 20:08:31 +0300

    04-copy-direction

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourceDir = path.join(__dirname, 'files');
+const newDir = path.join(__dirname, 'files-copy');
+
+fs.stat(newDir, (err, stats) => {
+    if(!stats){
+        fs.mkdir(newDir, (err) => {
+            if(err){
+                throw err;
+            }
+            copyDir();
+        });
+    }else{
+        copyDir();
+    }
+});
+
+function copyDir() {
+    fs.readdir(sourceDir, (err, files) => {
+      if (err) {
+        throw err;
+      }
+  
+      files.forEach((file) => {
+        const sourceFile = path.join(sourceDir, file);
+        const copyFile = path.join(newDir, file);
+  
+        fs.copyFile(sourceFile, copyFile, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    });
+  }
+  
 
